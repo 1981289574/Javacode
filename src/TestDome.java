2f0d0a11949bb188267f354b9d0ea9b88f2b4ce6
@cwh_99 <1981289574@qq.com>
2021-10-19 23:57:54 +0800

    idea快捷键

---
+++ src/TestDome.java
@@ -0,0 +1,40 @@
+public class TestDome {
+
+   /* public static void main(String[] args) {//psvm回车打印主函数
+        int a=10;
+        int b=20;
+        System.out.println("a+b");
+
+
+        int ret=a+b;
+        System.out.println(ret);
+        System.out.println("hello");//sout回车打印输出
+    }*/
+    //块注释
+
+//idea快捷键
+    public static void main(String[] args) {
+    //psvm回车主函数
+        System.out.println("hello");//sout打印hello
+        for (int i = 0; i < 10; i++) {//fori输出一个for循环,补充n的值
+            System.out.println("abcd");
+        }
+        int a=10;
+        System.out.println(a);//a.sout可直接打印a,先对a定义数据类型即可快捷使用a.sout
+        int b=100000;
+        int c=10_0000;
+        System.out.println(b);
+        System.out.println(c);//10万的易观察
+        //文档注释（块注释）,全部选中ctrl+shift+/
+        //取消块注释：ctrl+shift+/
+        //     /**
+        //      *
+        //      *///文档注释
+        //     /*
+        //      块注释
+        //     */
+        //行注释：ctrl+/
+        //取消行注释：ctrl+/*/
+    }
+        }
+
